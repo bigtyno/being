5f029937a7c50b4ac30f5720cccfade1682c3314
bigtyno <bigtyno2@gmail.com>
2021-08-07 14:41:28 +0900

    ..

---
+++ beingInterior/src/main/java/org/zerock/mapper/InteriorMapper.java
@@ -0,0 +1,35 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.Criteria;
+import org.zerock.domain.FileVO;
+import org.zerock.domain.InteriorFileVO;
+import org.zerock.domain.InteriorVO;
+
+public interface InteriorMapper {
+
+	public void create(InteriorVO interior) throws Exception;
+
+	public List<InteriorVO> selectInteriorList() throws Exception;
+
+	public InteriorVO read(Integer num) throws Exception;
+
+	public List<InteriorVO> listPage(int page) throws Exception;
+
+	public List<InteriorVO> listCriteria(Criteria cri) throws Exception;
+
+	public int countPaging(Criteria cri) throws Exception;
+
+	public void update(InteriorVO vo) throws Exception;
+
+	public void delete(Integer num) throws Exception;
+	
+	public void insertInteriorFile(InteriorFileVO interiorfileVO) throws Exception;
+	
+	public List<InteriorFileVO> selectInteriorFileList(Integer num) throws Exception;
+	
+	public void deleteInteriorFile(Integer num) throws Exception;
+	
+	public InteriorFileVO selectInteriorFileList1(Integer num) throws Exception;
+}
---
+++ beingInterior/src/main/java/org/zerock/service/InteriorService.java
@@ -0,0 +1,30 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.zerock.domain.InteriorVO;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.InteriorFileVO;
+
+public interface InteriorService {
+
+	public void create(InteriorVO interior, List<InteriorFileVO> filelist) throws Exception;
+
+	public List<InteriorVO> selectInteriorList() throws Exception;
+
+	public InteriorVO read(Integer num) throws Exception;
+
+	public List<InteriorVO> listPage(int page) throws Exception;
+
+	public List<InteriorVO> listCriteria(Criteria cri) throws Exception;
+
+	public int listCountCriteria(Criteria cri) throws Exception;
+
+	public void modify(InteriorVO interior) throws Exception;
+
+	public void remove(Integer num) throws Exception;
+	
+	public List<InteriorFileVO> selectInteriorFileList(Integer num) throws Exception;
+	
+	//public InteriorFileVO selectInteriorFileList1(Integer num) throws Exception;
+}
